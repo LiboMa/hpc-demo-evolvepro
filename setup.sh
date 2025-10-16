4a3c3c76d5944206a9c25f953075192244757968
AWS ParallelCluster user <ubuntu@ip-10-251-55-82.us-west-2.compute.internal>
2025-10-16 09:46:40 +0000

    update

--- setup.sh
+++ setup.sh
@@ -11,11 +11,11 @@ bash -x Anaconda3-2025.06-1-Linux-x86_64.sh
 }
 
 ## create conda env
-#
 
 setup_conda() {
 
 cd /shared/EvolvePro
+## this part will take 5-10 mins
 conda env create -f environment.yml
 conda activate evolvepro
 
@@ -25,11 +25,14 @@ conda init
 conda activate evolvepro
 pip install -e .
 python -m pip install --upgrade jupyter
-
 pwd
+
 }
 
+## setup jupyter service
+#
 setup_jupyter_service () {
+
 cd /shared
 sudo cp juypter_systemd_config/jupyterlab.service /etc/systemd/system/
 sudo systemctl enable jupyterlab
@@ -37,6 +40,11 @@ sudo systemctl daemon-reload && sudo systemctl restart jupyterlab
 }
 
 
+## addition install
+#
+sudo apt -y install seqtk
+
+
 install_conda
-#setup_condg
+setup_condg
 setup_jupyter_service
